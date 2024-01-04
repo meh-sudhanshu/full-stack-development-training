1865d98a1c6d0bdb2402228187c723b506e4547e
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-01-04 15:29:16 +0530

    more functionality

--- Html/flexbox/NumberGame/script.js
+++ Html/flexbox/NumberGame/script.js
@@ -18,8 +18,23 @@ console.log(buttons[1])
 
 function increaseHandler(){
     number = Number(number)
+    if(number+1 > 10){
+        alert("number can not go beyond 10")
+        return
+    }
     number = number+1
     headingTag.innerHTML = number
 }
 
+function decreaseHandler(){
+    number = Number(number)
+    if(number-1 < -10){
+        alert("number can not go below -10")
+        return
+    }
+    number = number-1
+    headingTag.innerHTML = number
+}
+
 increaseButton.addEventListener("click",increaseHandler)
+decreaseButton.addEventListener("click",decreaseHandler)
