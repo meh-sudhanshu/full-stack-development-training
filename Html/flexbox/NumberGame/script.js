db1e08a02f596e79901c02c212692fb8e46adb66
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-01-06 10:59:43 +0530

    number colpleted

--- Html/flexbox/NumberGame/script.js
+++ Html/flexbox/NumberGame/script.js
@@ -16,10 +16,15 @@ console.log(buttons[0])
 console.log(buttons[1])
 
 
+const overlay = document.getElementsByClassName("overlay")[0]
+
+const closeButton = document.getElementById("close_btn")
+
+
 function increaseHandler(){
     number = Number(number)
     if(number+1 > 10){
-        alert("number can not go beyond 10")
+        overlay.classList.remove("hidden")
         return
     }
     number = number+1
@@ -29,12 +34,20 @@ function increaseHandler(){
 function decreaseHandler(){
     number = Number(number)
     if(number-1 < -10){
-        alert("number can not go below -10")
+        overlay.classList.remove("hidden")
         return
     }
     number = number-1
     headingTag.innerHTML = number
 }
 
+
+function closeButtonHandler(){
+    overlay.classList.add("hidden")
+}
+
 increaseButton.addEventListener("click",increaseHandler)
 decreaseButton.addEventListener("click",decreaseHandler)
+
+
+closeButton.addEventListener("click",closeButtonHandler)
