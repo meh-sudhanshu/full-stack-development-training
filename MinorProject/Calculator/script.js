173e394c3274f4c95e6169310eb07f407cdbef35
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-01-11 14:38:06 +0530

    equation evaluation functionality impl

--- MinorProject/Calculator/script.js
+++ MinorProject/Calculator/script.js
@@ -3,10 +3,19 @@
 
 const keyBoard = document.getElementsByClassName("bottom")[0]
 const equationH2 = document.getElementsByClassName("equation")[0]
+const outputH2 = document.getElementsByClassName("output")[0]
 
 
 
 const keyboardHandler = (event)=>{
+    console.log(event)
+    if(String(event.target.textContent) === "="){
+        console.log(eval(equationH2.innerHTML))
+        outputH2.innerHTML = eval(equationH2.innerHTML)
+        return
+    }
+
+
     if(String(event.target.textContent) === "C"){
         equationH2.innerHTML = ""
         return
