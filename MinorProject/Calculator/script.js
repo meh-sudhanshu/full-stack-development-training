c34bd449ed8c98a1d803b655eecc439882c9f3ff
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-01-11 14:51:07 +0530

    calculator development completed

--- MinorProject/Calculator/script.js
+++ MinorProject/Calculator/script.js
@@ -4,6 +4,10 @@
 const keyBoard = document.getElementsByClassName("bottom")[0]
 const equationH2 = document.getElementsByClassName("equation")[0]
 const outputH2 = document.getElementsByClassName("output")[0]
+const historyEquation = document.getElementsByClassName("history-equation")[0] 
+
+historyEquation.innerHTML = localStorage.getItem("history")
+
 
 
 
@@ -12,6 +16,8 @@ const keyboardHandler = (event)=>{
     if(String(event.target.textContent) === "="){
         console.log(eval(equationH2.innerHTML))
         outputH2.innerHTML = eval(equationH2.innerHTML)
+        localStorage.setItem("history",String(equationH2.innerHTML))
+        historyEquation.innerHTML = localStorage.getItem("history")
         return
     }
 
